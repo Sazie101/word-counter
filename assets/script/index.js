30b1902e7ca7e80dfec519ccfe2216a6ce22395b
Samuel Yazie <samuelwassie169@gmail.com>
2023-11-08 12:46:53 -0600

    Creating repo

---
+++ assets/script/index.js
@@ -0,0 +1,60 @@
+'use strict';
+
+function onEvent(event, selector, callback) {
+    return selector.addEventListener(event, callback);
+}
+
+function selectById(selector, parent = document) {
+    return parent.getElementById(selector);
+}
+
+function select(selector, parent = document) {
+    return parent.querySelector(selector);
+}
+
+function create(element, parent = document) {
+    return parent.createElement(element);
+}
+
+function print(...args) {
+    console.log(args.join(', '));
+}
+
+const text = selectById('input');
+const characterCounter = selectById('characters');
+const wordCounter = selectById('words');
+const reset = selectById('reset');
+
+function checkCharacters() {
+    let characterCount = text.value.length;
+    let textArr = text.value.trim().split(' ');
+    let wordArr = textArr.filter(word => word !== '');
+    let wordCount = wordArr.length;
+    return {characterCount, wordCount};
+};
+
+onEvent('keyup', text, () => {
+    reset.classList.add('visible');
+    if (text.value === '') {
+        reset.classList.remove('visible');
+    }
+    const {characterCount, wordCount} = checkCharacters();
+    characterCounter.innerText = characterCount;
+    wordCounter.innerText = wordCount;
+});
+
+onEvent('load', window, () => {
+    text.value = '';
+    const {characterCount, wordCount} = checkCharacters();
+    characterCounter.innerText = characterCount;
+    wordCounter.innerText = wordCount;
+});
+
+onEvent('click', reset, (event) => {
+    event.preventDefault();
+    reset.classList.remove('visible');
+    text.value = '';
+    const {characterCount, wordCount} = checkCharacters();
+    characterCounter.innerText = characterCount;
+    wordCounter.innerText = wordCount;
+});
